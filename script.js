a55a9546698d8a3e1356f77a677e3d98bb7b04b1
raj_mishra <rajkiit116@gmail.com>
2018-02-13 22:51:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('form');
+var inputTxt = document.querySelector('.txt');
+var voiceSelect = document.querySelector('select');
+
+var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');
+
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', 'hi-IN');
+    option.setAttribute('data-name', 'Google हिन्दी');
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function speak(){
+    if (synth.speaking) {
+        console.error('speechSynthesis.speaking');
+        return;
+    }
+    if (inputTxt.value !== '') {
+    var utterThis = new SpeechSynthesisUtterance('Ranjita,.  mujhse naa ho paayegaa');
+    utterThis.onend = function (event) {
+        console.log('SpeechSynthesisUtterance.onend');
+    }
+    utterThis.onerror = function (event) {
+        console.error('SpeechSynthesisUtterance.onerror');
+    }
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.pitch = 1.0;
+    utterThis.rate = 0.9;
+    synth.speak(utterThis);
+  }
+  
+}
+
+inputForm.onsubmit = function(event) {
+  event.preventDefault();
+
+  speak();
+
+  inputTxt.blur();
+}
+
+pitch.onchange = function() {
+  pitchValue.textContent = 0.8;
+}
+
+rate.onchange = function() {
+  rateValue.textContent = 0.9;
+}
+
+voiceSelect.onchange = function(){
+  speak();
+}
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  inputTxt.remove();rate.remove();rateValue.remove();pitch.remove();pitchValue.remove();voiceSelect.remove();
+});
